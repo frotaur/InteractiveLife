35a955a3f423cf62b7c7ddbf32ddd19331d03b06
Vassilis <frotaur@hotmail.co.uk>
2026-07-21 15:02:40 +0200

    Bunch of functionality, cleaning up

--- Automaton.py
+++ Automaton.py
@@ -1,11 +1,12 @@
 import numpy as np
-from numba import njit, prange,cuda 
-import numba.typed as numt
+from numba import njit, prange 
 import random
 from PIL import Image, ImageEnhance
+import torch
+from torchenhanced import DevModule
+from torchenhanced.util import showTens
 
-
-class Automaton :
+class Automaton(DevModule) :
     """
         Class that internalizes the rules and evolution of 
         the cellular automaton at hand. It has a step function
@@ -21,15 +22,20 @@ class Automaton :
         
     """
 
-    def __init__(self,size):
-        self.w, self.h  = size
+    def __init__(self,size,device='cpu'):
+        super().__init__(device)
+        self.h, self.w  = size
         self.size= size
         self._worldmap = np.zeros((self.w,self.h,3))
-    
+
+        self.to(device)
 
     def step(self):
         return NotImplementedError('Please subclass "Automaton" class, and define self.step')
     
+    def draw(self):
+        # Should compute _worldmap given state
+        pass
     @property
     def worldmap(self):
         return (255*self._worldmap).astype(dtype=np.uint8)
@@ -63,14 +69,11 @@ class SMCA(Automaton):
         super().__init__(size)
         # 0,1,2,3 are  N,O,S,E directions
         self.particles = np.random.randn(1,self.w,self.h) # (1,W,H)
-        self.particles = np.where(self.particles>1.9,1,0).astype(np.int16)
+        self.particles = np.where(self.particles>1.9,1,0).astype(np.int)
 
         self.rule=self.convertxy(rule)
 
-        self.dir = np.array([[0,-1],[-1,0],[0,1],[1,0]])
 
-        self.emission_p = 1.
-        self.interaction_p = 1.
 
         self.brush_size=3
 
@@ -78,6 +81,7 @@ class SMCA(Automaton):
         self.limmax = np.array([self.w,self.h])
 
         self.background = np.array([35.,35.,51.])/255.
+
     def evolve_step(self):
         self.particles=evolve_cpu(self.particles,self.w,self.h,self.rule[0],self.rule[1])
         
@@ -178,22 +182,183 @@ def evolve_cpu(partics,w,h,rulex,ruley) :
     return newpartics
 
 
-# NICE BUT WRONG
-# @njit(parallel=True)
-# def evolve_cpu(partics,w,h,rulex,ruley) :
-#     newpartics=np.copy(partics)
-#     for x in prange(w):
-#         for y in prange(h):
-#             sum=0
-#             for i in [-1,0,1]:
-#                 for j in [-1,0,1]:
-#                     if(i!=0 or j!=0):
-#                         sum+=partics[0,(x+i)%w,(y+j)%h]
-#             if(partics[0,x,y]==1):
-#                 if((rulex >> sum) & 1):
-#                     newpartics[0,x,y]=1
-#             if(partics[0,x,y]==0):
-#                 if((ruley >> sum) & 1):
-#                     newpartics[0,x,y]=1
+class LifeLikeCA(Automaton) :
+    def __init__(self,size, rule :tuple[str], device='cpu'):
+        super().__init__(size)
+        self.h,self.w = size
+        self.size = torch.tensor(size)
+        self.x,self.y = self.to_unreadable([rule[0]],[rule[1]])
 
-#     return newpartics
+        self.state = self.get_init_mat_varied(batch_size=1,portion_range=(0.5,1.)) # (1,H,W)
+
+        self.background = np.array([35.,35.,51.])/255.
+
+        print('the world state is : ', self.state.shape)
+    
+    def set_rule(self,rule):
+        self.x,self.y = self.to_unreadable([rule[0]],[rule[1]])
+    def get_init_mat(self,rand_portion,flip_bw=False,batch_size=1):
+        """
+            Get initialization matrix for CA
+
+            Params : 
+            rand_portion : tensor, portion of the screen filled with noise.
+            flip_bw : bool, flips black and white.
+            batch_size : int, number of initial conditions to generate.
+        """
+        batched_size = torch.tensor([batch_size,self.h,self.w])
+        randsize = (batched_size[1:]*rand_portion).to(dtype=torch.int) # size of the random square
+        randstarts = (batched_size[1:]*(1-rand_portion)/2).to(dtype=torch.int) # Where to start the index for the random square
+
+        randsquare = torch.where(torch.randn((batch_size,*randsize.tolist()))>0,1,0) # Creates random square of 0s and 1s
+
+        init_mat = torch.zeros((batch_size,self.h,self.w),dtype=torch.int)
+        init_mat[:,randstarts[0]:randstarts[0]+randsize[0],
+        randstarts[1]:randstarts[1]+randsize[1]] = randsquare
+        init_mat = init_mat.to(torch.int)
+
+        if(flip_bw):
+            init_mat=1-init_mat
+
+        return init_mat.to(device=self.device, dtype=torch.int) # (B,H,W)
+
+    def get_init_mat_varied(self,batch_size=1,portion_range:tuple=(0.5,1.)):
+        """
+             initalization matrix (B,H,W), where each example is varied in terms
+             of the portion of the screen filled with random noise. Also flips b/w
+             randomly.
+
+             Args :
+             batch_size : self-explanatory
+             portion_range : range in which to generate the portions of noise for the inital condition
+        """
+            # Convert portions to a tensor
+        portions_tensor = torch.rand((batch_size,))*(portion_range[1]-portion_range[0])+portion_range[0]
+
+        # Calculate the size of the square for the entire batch
+        square_H = (self.h * portions_tensor).int()
+        square_W = (self.w * portions_tensor).int()
+
+        # Calculate starting and ending indices for the height and width for the entire batch
+        start_H = ((self.h - square_H) // 2).int()
+        end_H = (start_H + square_H).int()
+
+        start_W = ((self.w - square_W) // 2).int()
+        end_W = (start_W + square_W).int()
+
+        # Create a coordinate grid for height and width
+        rows = torch.arange(self.h).view(1, self.h, 1).expand(batch_size, -1, self.w)
+        cols = torch.arange(self.w).view(1, 1, self.w).expand(batch_size, self.h, -1)
+
+        # Create the masks based on computed indices
+        mask = ((rows >= start_H[:,None,None]) & (rows < end_H[:,None,None]) & (cols >= start_W[:,None,None]) & (cols < end_W[:,None,None])).float() # 1 if in correct portion
+
+        init_mat = (torch.randint(0,2,size=(batch_size,self.h,self.w))*mask).int()
+
+        flip_bw = (torch.randn(size=(batch_size,))>0)[:,None,None].expand(-1,self.h,self.w)
+
+        init_mat = torch.where(flip_bw,init_mat,1-init_mat)
+
+        return init_mat.to(device=self.device, dtype=torch.int) # (B,H,W)
+    
+    def evo_step(self,mat,x:int,y:int):
+        """
+            Evolves one step, using the 'un-readable' convention for rules x and y. In binary, the presence of 1 in the location d of x(y) means
+            that if a live(dead) cell has d live neighbors, then it will survive(be born).
+
+            params :
+            mat : (1,H,W) matrix of state
+            x,y : ints, rules for survival and births respectively.
+        """
+        mat = mat[0]
+        wmat, emat = mat.roll(-1, 0), mat.roll(1, 0) # the second argument is the roll axis
+        nmat, smat = mat.roll(-1, 1), mat.roll(1, 1) 
+        swmat, semat = wmat.roll(1, 1), emat.roll(1, 1)
+        nwmat, nemat = wmat.roll(-1, 1), emat.roll(-1, 1)
+
+        count_mat = wmat + emat + nmat + smat + swmat + semat + nwmat + nemat
+
+        return torch.where(mat==1,self.get_nth_bit(x,count_mat),self.get_nth_bit(y,count_mat)).to(torch.int)[None]
+
+    def step(self):
+        self.state = self.evo_step(self.state,self.x,self.y)
+
+    def to_unreadable(self,x : list[str],y : list[str]):
+        """
+            Transform classic notation for x/y (see wikipedia article), to the more convenient notation
+            that I designed, explained in evo_step. EX : S23/B3 is game of life, translated to S12/B8 in 
+            my notation.
+
+            <MAYBE LATER CHANGE TO TREAT RULES AS TUPLES (X,Y) INSTEAD OF SEPARATELY>
+            params :
+            x,y : list of rules in classic notation.
+
+            returns :
+            x,y : tensor of rules in my notation.            
+        """
+        x_out = []
+        y_out = []
+
+        # Iterate over x and y
+        for rule in x:
+            if(rule==''):
+                x_out.append(0)
+            else:
+                x_out.append(sum([2**int(d) for d in rule]))
+        
+        for rule in y:
+            if(rule==''):
+                y_out.append(0)
+            else:
+                y_out.append(sum([2**int(d) for d in rule]))
+        
+        print('got rules : ', x_out, y_out)
+        return torch.tensor(x_out,dtype=torch.int),torch.tensor(y_out,dtype=torch.int)
+
+    def get_nth_bit(self,num, n):
+        """
+        Returns the nth bit (counting from the right, starting at 0) of num in binary representation.
+            n can be a tensor of integers. Num can ALSO be a tensor
+        """
+
+        return (num>>n) & 1
+
+    def draw(self):
+        """
+            Updates the worldmap with the current state.
+        """
+        self._worldmap[:,:,1] -= 0.07
+        self._worldmap[:,:,2] -= 0.15
+        self._worldmap[:,:,0] -= 0.01
+        self._worldmap[self._worldmap<0] =0.
+
+        self._worldmap[:,:,:]+=self.state[0].transpose(1,0)[:,:,None].cpu().numpy()
+        self._worldmap = np.minimum(self._worldmap,1)
+        self._worldmap = np.maximum(self._worldmap,self.background[None,None,:])
+
+    def state_from_picture(self,pic_loc):
+        """
+            Loads state from picture.
+        """
+        img = Image.open(pic_loc)
+        img = ImageEnhance.Brightness(img).enhance(0.5)
+        img = img.convert('L')
+        img.thumbnail((self.w,self.h))
+
+        img = (np.array(img))[None,:,:] # (1,H,W)
+        print('img shape : after transpo : ',img.shape)
+        # Calculate padding
+        pad_height = max(self.h - img.shape[1],0)
+        pad_width = max(self.w - img.shape[2],0)
+
+        # Ensure padding is equally split between top/bottom and left/right
+        pad_top = pad_height // 2
+        pad_bottom = pad_height - pad_top
+        pad_left = pad_width // 2
+        pad_right = pad_width - pad_left
+
+
+        img = np.pad(img, ((0,0), (pad_top, pad_bottom),(pad_left, pad_right)), mode='constant', constant_values=0)
+        assert img.shape==(1,self.h,self.w), f"img shape : {img.shape}, expected : {(1,self.h,self.w)}"
+
+        self.state = torch.tensor(np.where(img>55,1,0),device=self.device).to(torch.int)
--- main.py
+++ main.py
@@ -1,44 +1,84 @@
 import pygame
 from Camera import Camera
 from Automaton import *
-import cv2 
+import cv2, os
 
 
 
 def make_recording(frames,writer):
-    frames = frames[::-1]
+    if(invert_video):
+        frames = frames[::-1]
     for frame in frames :
         writer.write(frame)
-    print(f'there are {len(frames)} frames')
     writer.release()
+    
     print('DONE !')
 
 
+def make_writer():
+    global video, fourcc
+    video_dir = 'videos'
+    os.makedirs(video_dir, exist_ok=True)
+    numvids = len(os.listdir(video_dir))
+    video = cv2.VideoWriter(os.path.join(video_dir,f'{numvids}.mp4'), fourcc, 60, (W, H))
+
 pygame.init()
-W,H =1200,600
+# NOT full-HD display
+H,W = 460,640
+invert_video = True
+picture = 'titlescreen.png'
+
+
+
+
 screen = pygame.display.set_mode((W,H),flags=pygame.SCALED|pygame.RESIZABLE)
 clock = pygame.time.Clock()
 running = True
 camera = Camera(W,H)
 
+
+
 world_state = np.random.randint(0,255,(W,H,3),dtype=np.uint8)
 
-rules = {'chaosblob':("5678","25678"), 'life': ("23","3")}
-auto = SMCA((W,H),rules['chaosblob'])
+rules = {'chaosblob':("5678","25678"), 'life': ("23","3"), 'gnarl':("1","1"), 'replicator':("1357","1357"),
+         'maze':("12345","3"),"coral":("45678","3"),"coagulations":("235678","378"),"walled_cities":("45678","45678"),
+        "amoeba":("1358","357"),"assimilation":("4567","345"),"diamoeba":("5678","3567"),"day_and_night":("34678","3678"),
+        "highlife":("23","36"),"stains":("235678","3678"),"2x2":("36","125"),"34":("34","34"),"longlife":("5","345"),"move":("245","368"),
+        "pseudo_life":("357","238"),"serviettes":("234","3"),"stains":("235678","3678"),"vote":("5678","45678"),"walled_maze":("1234","3"),
+        "replicator":("1357","1357"),"2x2":("36","125"),"34":("34","34"),"amoeba":("1358","357"),"assimilation":("4567","345"),
+        "coagulations":("235678","378"),"coral":("45678","3"),"day_and_night":("34678","3678"),"diamoeba":("5678","3567"),
+        "flakes":("012345678","3"),"gnarl":("1","1"),"highlife":("23","36"),"life":("23","3"),"longlife":("5","345"),
+        "maze":("12345","3"),"move":("245","368"),"pseudo_life":("357","238"),"serviettes":("234","3"),"stains":("235678","3678"),
+        "vote":("5678","45678"),"walled_cities":("45678","45678"),"walled_maze":("1234","3"),"ringsandslugs":("14568","56"),
+         "vote45":("35678","4678") }
+
+newrules = {'feux':("0247","1358"), 'snakeskin':("134567","1")}
+rulelist = list(rules.values())
+rulenum = 0
+
+picturelist= [os.path.join('oldscreens',pic) for pic in os.listdir('oldscreens')]
+picturenum = 0
+auto = LifeLikeCA((H,W),rules['life'],device='cpu')
 
 stopped=True
+
+
 add_drag = False
 rem_drag = False
 recording=False
 
-auto.state_from_picture('TitleTest.png')
+auto.state_from_picture('pic.jpg')
 
 
-fourcc =  cv2.VideoWriter_fourcc(*'VP80')  # 'mp4v' is a codec that works with .mp4 files. For .avi files, use 'XVID'
-video = cv2.VideoWriter('animation.webm', fourcc, 24, (W, H))
+fourcc =  cv2.VideoWriter_fourcc(*'H264')  # 'mp4v' is a codec that works with .mp4 files. For .avi files, use 'XVID'
+video = None
+
+make_writer()
 
 frames=[]
 
+fuckface = [(472, 24), (355,401), (496,346), (447,148), (198,360)]
+fucki = 0
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -68,22 +108,48 @@ while running:
             if(event.key == pygame.K_SPACE):
                 stopped=not(stopped)
             if(event.key == pygame.K_r):
-                recording = not recording
+                if(recording):
+                    make_recording(frames,video)
+                    frames=[]
+                    recording=False
+                    make_writer()
+                    print('DONE !')
+                else:
+                    recording = True
                 print('REC')
-            
+            if(event.key == pygame.K_i):
+                auto.state_from_picture(picture)
+            if(event.key == pygame.K_s):
+                auto.set_rule(rulelist[rulenum])
+                rulenum = (rulenum+1)%len(rulelist)
+            if(event.key == pygame.K_n):
+                picture = picturelist[picturenum]
+                picturenum = (picturenum+1)%len(picturelist)
+                auto.state_from_picture(picture)
+            if(event.key == pygame.K_d):
+                # auto.x = torch.tensor(np.random.randint(0,512,(1,)),dtype=torch.int,device=auto.device)
+                # auto.y = torch.tensor(np.random.randint(0,512,(1,)),dtype=torch.int,device=auto.device)
+                fucker = fuckface[fucki]
+                auto.x = torch.tensor([fucker[0]])
+                auto.y = torch.tensor([fucker[1]])
+                fucki+=1
+                print('rule : ', auto.x.cpu().item(), auto.y.cpu().item())
 
     if(not stopped):
         auto.step()
-    auto.update_map()
+    auto.draw()
     world_state = auto.worldmap
     surface = pygame.surfarray.make_surface(world_state)
 
     if(recording):
-         frame = world_state.transpose(1,0,2)
-         tempB = np.copy(frame[:,:,2])
-         frame[:,:,2]=frame[:,:,0]
-         frame[:,:,0]=tempB
-         frames.append(world_state.transpose(1,0,2))
+        frame = world_state.transpose(1,0,2)
+        tempB = np.copy(frame[:,:,2])
+        frame[:,:,2]=frame[:,:,0]
+        frame[:,:,0]=tempB
+        frames.append(world_state.transpose(1,0,2))
+
+        # Draw a red rectangle on the bottom right
+        pygame.draw.rect(surface, (255, 0, 0), (W-50, H-50, 10, 10))
     # Clear the screen
     screen.fill((0, 0, 0))
 
@@ -96,7 +162,7 @@ while running:
     pygame.display.flip()
     # flip() the display to put your work on screen
 
-    clock.tick(24)  # limits FPS to 60
+    clock.tick(60)  # limits FPS to 60
 
 
 pygame.quit()
